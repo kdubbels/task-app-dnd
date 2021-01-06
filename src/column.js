3f457058632b970c6de1d95464bab151f034c386
Kristofer Dubbels <dubbk381@alumni.newschool.edu>
2021-01-06 16:45:32 -0700

    first commit

---
+++ src/column.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Droppable } from 'react-beautiful-dnd';
+import Task from './task';
+
+const Container = styled.div`
+    margin: 8px;
+    border: 1px solid lightgrey;
+    border-radius: 2px;
+`;
+const Title = styled.h3`
+    padding: 8px;
+`;
+const TaskList = styled.div`
+    padding: 8px;
+`;
+
+export default class Column extends React.Component {
+    render() {
+        return (
+            <Container>
+            <Title>{this.props.column.title}</Title>
+                <Droppable droppableId={this.props.column.id}>
+                   {provided => {
+                        <TaskList
+                            innerRef={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            {this.props.tasks.map(task => <Task key={task.id} task={task} />)}
+                            {provided.placeholder}
+                        </TaskList>
+                    }}
+                </Droppable>
+            </Container>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import '@atlaskit/css-reset';
 import reportWebVitals from './reportWebVitals';
+import { DragDropContext } from 'react-beautiful-dnd';
+import initialData from './initial-data';
+import Column from './column';
+
+
+class App extends React.Component {
+  state = initialData;
+
+  onDragEnd = result => {
+    
+  }
+
+  render() {
+    return (
+      <DragDropContext
+        // onDragStart
+        // onDragUpdate
+        onDragEnd={this.onDragEnd}
+      >
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks} />
+        })}
+      </DragDropContext>
+    )
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
